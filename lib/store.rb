858e0442d2b87625929a0a56c5b397b78aad9884
paul <pmorin35@gmail.com>
2015-06-19 10:01:21 -0700

    allow store name to be capitalized automatically. Passes all specs

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,10 @@
 class Store <ActiveRecord::Base
   has_and_belongs_to_many(:shoes)
+  validates(:name, :presence => true)
+  before_save(:capitalize_name)
+
+  private
+  define_method(:capitalize_name) do
+    self.name = (name().capitalize())
+  end
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -2,4 +2,15 @@ require "spec_helper"
 
 describe(Store) do
   it { should have_and_belong_to_many(:shoes)}
+
+  it("validates presence of name") do
+    new_store = Store.new({:name => ""})
+    expect(new_store.save()).to(eq(false))
+  end
+
+  it("capitalizes entry when saved") do
+    new_store = Store.create({:name => "target"})
+    expect(new_store.name()).to(eq("Target"))
+  end
+
 end
