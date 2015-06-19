66b8f20b36f5e4db5eb673dbdae272a38fc7a0b7
paul <pmorin35@gmail.com>
2015-06-19 10:18:20 -0700

    index page and app.rb

---
+++ app.rb
@@ -0,0 +1,9 @@
+require("bundler/setup")
+Bundler.require(:default)
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @shoes = Shoe.all()
+  @stores = Store.all()
+  erb(:index)
+end
