011438699b4ae99a262d6f20adee4d5a8f699f01
paul <pmorin35@gmail.com>
2015-06-19 09:43:09 -0700

    allow brand to be automatically capitalized and pass all specs.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,10 @@
 class Shoe <ActiveRecord::Base
   has_and_belongs_to_many(:stores)
+  validates(:brand, :presence => true)
+  before_save(:capitalize_brand)
+
+  private
+    define_method(:capitalize_brand) do
+      self.brand = (brand().capitalize())
+    end
 end
--- spec/shoe_spec.rb
+++ spec/shoe_spec.rb
@@ -2,4 +2,14 @@ require "spec_helper"
 
   describe(Shoe) do
     it { should have_and_belong_to_many(:stores) }
+
+    it("validates presence of brand") do
+      new_shoe = Shoe.new({:brand => ""})
+      expect(new_shoe.save()).to(eq(false))
+    end
+
+    it("capitalizes entry when saved") do
+      new_shoe = Shoe.create({:brand => "nike"})
+      expect(new_shoe.brand()).to(eq("Nike"))
+    end
   end
